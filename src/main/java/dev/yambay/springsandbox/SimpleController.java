eb202a96bd9a9be062486a5ce56aea69f98ee264
Ilyas Yambay <ilyas.yambay@gmail.com>
2024-03-17 19:38:28 +0100

    Add: Let controller answer with own UUID.

--- src/main/java/dev/yambay/springsandbox/SimpleController.java
+++ src/main/java/dev/yambay/springsandbox/SimpleController.java
@@ -3,11 +3,14 @@ package dev.yambay.springsandbox;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 public class SimpleController {
+    private static final String SOME_UUID = java.util.UUID.randomUUID().toString();
 
     @GetMapping("/hello-world")
     public String helloWorld() {
-        return "Hello world!";
+        return "%s: Hello world!".formatted(SOME_UUID);
     }
 }
